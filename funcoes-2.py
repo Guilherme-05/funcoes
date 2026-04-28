5dc367f93c4a026c84974969850d9c87e72469e1
Guilherme <guilhermemazzo9@gmail.com>
2026-04-28 14:47:35 -0300

    Mudança de nome do arquivo

---
+++ funcoes-2.py
@@ -0,0 +1,13 @@
+# Parâmetros com valor padrão (default)
+def saudacao(nome, mensagem="Bem vindo!"):
+    print(f"Olá {nome}! {mensagem}")
+
+saudacao("Guilherme")
+saudacao("Wederson", "Boa noite!")
+
+#Argumentos nomeados (keyword args)
+def criar_usuario(nome, idade, admin = False):
+    print(f"{nome} | {idade} Anos | Admin = {admin}")
+
+criar_usuario(idade=20, nome="Guilherme")
+criar_usuario("Wederson", 45, admin = True)
