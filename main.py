bad254fbbdf1171a2b33b1eea486c0c903394319
Guilherme <guilhermemazzo9@gmail.com>
2026-04-28 14:17:42 -0300

    Função de formatar + Colocar um input de informar valor.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("bom dia")
+def formatar_dinheiro_PraReal(valor):
+    texto = f"R$ {valor:,.2f}" # Padrão EUA 1,200.40
+    texto = texto.replace(",", "X")
+    texto = texto.replace(".", ",")
+    texto = texto.replace("X", ".")
+    return texto
+
+preco = int(input("Informe o valor: "))
+print(formatar_dinheiro_PraReal(preco))
